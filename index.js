ff5571195b037c30b222ff846f7102a70e086f2a
Nice <41243187+niceblu3@users.noreply.github.com>
2019-10-11 13:59:04 -0500

    Index con puerto correcto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 const { importSchema } = require('graphql-import');
 const { makeExecutableSchema } = require('graphql-tools');
 const mongoose = require('mongoose');
@@ -17,46 +17,6 @@ mongo.on('error', (error) => console.log(error))
 
 const typeDefs = importSchema(__dirname + '/schema.graphql');
 
-// const typeDefs = `
-
-//     type Query{
-//         hello(name:String):String!
-//         getUsers:[User!]
-
-//     }
-
-//     type Mutation{
-//         createUser(name:String!,age:Int!):User
-//     }
-
-//     type User{
-//         id:Int!
-//         name:String!
-//         age:Int!
-//     }
-
-// `;// La definición de como van a ser recibidos y enviados los
-//datos
-// const users = [];
-
-// const resolvers = {
-//     Query:{
-//         hello:(root,params,context,info) => `Hola ${params.name}`,//interpolacion
-//         getUsers:(root,params,context,info) => users,
-
-//     },
-//     Mutation:{
-//         createUser:(root,params,context,info) => {
-//             const user  = {id:users.length+1,name:params.name,age:params.age};
-//             users.push(user);
-//             return user;
-//         },
-//     }
-// };
-//root -> trae la info del servidor de Graphql
-//params -> son los datos que envia el cliente y estan definidos en el typeDefs
-//context -> objeto por el cual se comunican todos los resolvers(Auth)
-//info -> El query que se ejecuto en el cliente
 
 const schema = makeExecutableSchema({
     typeDefs,
